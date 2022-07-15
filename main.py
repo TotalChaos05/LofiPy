7eeeb426a1d5e35d9569f14b3f82a918e0186102
Basil <basil.keeler@outlook.com>
2022-07-15 18:39:08 -0500

    e

--- main.py
+++ main.py
@@ -1,16 +1,85 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sys
+import vlc
+import pafy
+import keyboard
+import tkinter as tk
+import tkinter.ttk as ttk
+import time
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def menu():
+    window = tk.Tk()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    window.title("LofiPy")
+    window.geometry('500x700')
+    window.tk.call('source', 'forest-light.tcl')
+    ttk.Style().theme_use('forest-light')
+
+    def ok_button_command():
+
+        choice = sel.get()
+        customurl = text.get()
+        window.destroy()
+        if choice == 1:
+            playvideo('https://www.youtube.com/watch?v=jfKfPfyJRdk')
+
+        elif choice == 2:
+            playvideo('https://www.youtube.com/watch?v=rUxyKA_-grg')
+
+        elif choice == 3:
+            playvideo(customurl)
+            window.destroy()
+
+    sel = tk.IntVar()
+    text = tk.StringVar()
+
+    op1 = ttk.Radiobutton(window, style='TRadiobutton', value=1, text='lofi hip hop radio - beats to relax/study to', variable=sel)
+    op2 = ttk.Radiobutton(window, style='TRadiobutton', value=2, text='lofi hip hop radio - beats to sleep/chill to', variable=sel)
+    op3 = ttk.Radiobutton(window, style='TRadiobutton', value=3, text='custom livestream', variable=sel)
+    textInput = ttk.Entry(window, exportselection=False, textvariable=text)
+    ok_button = ttk.Button(window, style='TButton', text="Ok", command=ok_button_command)
+    instructions = tk.Text(window,)
+    instructions.pack(side='right')
+    instructions.insert(1.0, '''LofiPy 
+                        A dedicated app for playing Lofi Girl's YouTube Livestreams
+                        Esc: Exit Video Player
+                        F11: Fullscreen''')
+    op1.grid(column=0, row=0, sticky='w')
+    op2.grid(column=0, row=1, sticky='w')
+    op3.grid(column=0, row=2, sticky='w')
+    textInput.grid(column=1, row=2, sticky='w')
+    ok_button.grid(column=0, row=3)
+
+
+
+    window.mainloop()
+
+
+def playvideo(url):
+    video = pafy.new(url)
+    # getting best stream
+    best = video.getbest()
+    # creating vlc media player object
+    media = vlc.MediaPlayer(best.url)
+    # start playing video
+    media.play()
+
+    while True:
+        if keyboard.is_pressed('Esc'):
+            print('exiting...')
+            sys.exit(0)
+        elif keyboard.is_pressed('f11'):
+            vlc.libvlc_toggle_fullscreen(p_mi=media)
+            time.sleep(.5)
+        else:
+            #for some reason vlc doesn't like it when this isn't here
+            pass
+
+
+
+
+
+menu()
+
+
+
